4ecc83c6221183a5dedaec759ca44c4adb3b3a70
Alexandr <alex.mitin.psmu@gmail.com>
2020-10-27 23:58:42 +0500

    hello world

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ class OwnReact {
   static createElement(...args) {
     return [...args];
   }
+
+  static render(element, parentDom) {
+    // получение параметров элемента
+    const [type, props, content] = element;
+    // создание дом-элемента
+    const dom = document.createElement(type);
+    // добавляем контент к элементу
+    if (content) {
+      const text = document.createTextNode(content);
+      dom.appendChild(text);
+    }
+    // получение дочерних элементов
+    const childElements = props.children || [];
+    // рендер дочерних элементов
+    childElements.forEach(childElement => this.render(childElement, dom));
+    // вставка результата в DOM
+    parentDom.appendChild(dom);
+  }
 }
 
 export default OwnReact;
